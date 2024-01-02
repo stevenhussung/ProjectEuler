e7bacdca5d06da738731f354a7c4e09930c8072f
Steve Hussung <shussung@indot.in.gov>
2024-01-01 23:57:31 -0500

    Solving problem 37

--- scala-3-project-template/src/main/scala/Main.scala
+++ scala-3-project-template/src/main/scala/Main.scala
@@ -1,15 +1,39 @@
-@main def hello: Unit =
-    val engine = prime_engine
-    for i <- (-1 to 20) do
-        println((i,engine(i)))
+@main def problem_37: Unit =
+    val is_prime = prime_engine
 
-def msg = "I was compiled by Scala 3. :-)"
+    val truncateable_primes =
+        (1 to 1000000).filter(n => is_truncateable_prime(n, is_prime)).toList
+    
+    println(truncateable_primes)
+    println(truncateable_primes.sum)
+
+def is_truncateable_prime(n : Int, is_prime : Int => Boolean) : Boolean = 
+    if n < 10 then 
+        false
+    else
+        get_truncate_list(n) forall (is_prime)
 
 def prime_engine: (Int => Boolean) = 
-    def is_prime(p : Int) : Boolean =    
-        (2 to p - 1) forall (p % _ != 0)
+    def is_prime(n : Int) : Boolean =    
+        ( (2 to n - 1) forall (n % _ != 0) )
+        && (n > 1)
     
     val cache = collection.mutable.Map.empty[Int, Boolean]
 
+    n => cache.getOrElseUpdate(n, is_prime(n))
+    
+def get_truncate_list(n : Int) : List[Int] =
+    get_right_truncate_list(n)
+    ++ get_left_truncate_list(n)
 
-    return is_prime
+def get_right_truncate_list(n : Int) : List[Int] =
+    if n == 0 then
+        List()
+    else
+        get_right_truncate_list(n/10).appended(n)
+    
+def get_left_truncate_list(n : Int) : List[Int] =
+    val L_reversed : List[Int] = get_right_truncate_list(n.toString.reverse.toInt)
+    for n <- L_reversed
+        yield n.toString.reverse.toInt
+
