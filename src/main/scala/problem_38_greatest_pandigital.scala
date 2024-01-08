c22019d14525ecc999f295c4e76ce70debf583d2
Steve Hussung <shussung@indot.in.gov>
2024-01-08 00:12:57 -0500

    Solved problem 38 and added is_pandigital function

--- src/main/scala/problem_38_greatest_pandigital.scala
+++ src/main/scala/problem_38_greatest_pandigital.scala
@@ -1,24 +1,12 @@
 @main def main: Unit =
-    println("Checking in")
-    for i <- (1 to 100000) 
-    do
-        println(greatest_pandigital(i, 9))
+    println((1 to 9999).map(i => greatest_pandigital(i,9)).max)
 
-def greatest_pandigital(a : Int, n : Int) : Int = 
-    (1 to n).map(x => pandigital_str(a,x)).filter(_.length <= 9).map(_.toInt).max
+def greatest_pandigital(a : Int, n : Int) : Option[Int] = 
+    (2 to n).map(x => concatenated_products(a,x)).filter(_.length <= 9).map(_.toInt).filter(is_pandigital).reduceOption((a, b) => math.max(a, b))
 
-/*
-def greatest_pandigital(a : Int, n : Int, count_down : Int) : Int = 
-    if countdown == 0 then
-        0
-    else
-        math.max(
-            pandigital(a, n),
-            greatest_pandigital(a, n-1)
-        )
-        
-def pandigital(a : Int, n : Int) : Long = 
-    pandigital_str(a, n).tolong
-*/
-def pandigital_str(a : Int, n : Int) : String = 
+def concatenated_products(a : Int, n : Int) : String = 
     (1 to n).map(a*_).map(_.toString()).reduce((a, b) => a.concat(b))
+
+def is_pandigital(a : Int) : Boolean =
+    a.toString.toSet.size == 9
+    &&  !a.toString.toSet.contains('0')
