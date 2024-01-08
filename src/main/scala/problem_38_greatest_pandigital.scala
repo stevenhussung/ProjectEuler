193a82cbc881b150c564767ec985eb70870f2652
Steve Hussung <shussung@indot.in.gov>
2024-01-08 00:18:31 -0500

    Refactoring again

--- src/main/scala/problem_38_greatest_pandigital.scala
+++ src/main/scala/problem_38_greatest_pandigital.scala
@@ -2,11 +2,13 @@
     println((1 to 9999).map(i => greatest_pandigital(i,9)).max)
 
 def greatest_pandigital(a : Int, n : Int) : Option[Int] = 
-    (2 to n).map(x => concatenated_products(a,x)).filter(_.length <= 9).map(_.toInt).filter(is_pandigital).reduceOption((a, b) => math.max(a, b))
+    var products_list = (2 to n).map(x => concatenated_products(a,x)).filter(_.length <= 9).map(_.toInt)
+    products_list.filter(is_pandigital).reduceOption((a, b) => math.max(a, b))
 
 def concatenated_products(a : Int, n : Int) : String = 
     (1 to n).map(a*_).map(_.toString()).reduce((a, b) => a.concat(b))
 
 def is_pandigital(a : Int) : Boolean =
-    a.toString.toSet.size == 9
-    &&  !a.toString.toSet.contains('0')
+    var digit_set = a.toString.toSet
+    digit_set.size == 9
+    &&  !digit_set.contains('0')
