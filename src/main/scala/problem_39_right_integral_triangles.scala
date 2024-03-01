f1ee718acc7dc923d739b46ec828f6b535150ae8
Steve Hussung <steven.hussung@gmail.com>
2024-03-01 18:50:27 -0500

    Finishing problem 39

--- src/main/scala/problem_39_right_integral_triangles.scala
+++ src/main/scala/problem_39_right_integral_triangles.scala
@@ -1,25 +1,36 @@
 @main def problem_39 : Unit = 
     println("Here I go again")
-    for i <- (1 to 50) do
-        println(s"Is ${i} square? ${is_square(i)}")
-    
-    val max_perimeter : Int = 10
+
+    val max_perimeter : Int = 1000
     var perimeter_to_triangle_count : Map[Int, Int] = (1 to max_perimeter).map((_, 0)).toMap
 
 //Not quite sure how to iterate through these--are we computing c? Or iterating?
 //I think you want to compute c. Just check using is_square (or change is_square to return the two values)
+    val triangle_number_counts = 
     for a <- (1 to max_perimeter - 2)
-        b <- (1 to max_perimeter - a - 1)
-        c <- (1 to max_perimeter - a - b - 2)
-        do
-        // if is_triangle(a, b, c) then
-        println("test")
+        b <- (a to max_perimeter - a - 1)
+        c <- int_sqrt(a*a + b*b)
+        if a + b + c <= max_perimeter
+        yield
+        // println(s"a = ${a}, b = ${b}, a*a + b*b = ${a*a + b*b}, so c = ${c}, and a + b + c = ${a + b + c}")
+        a + b + c
+
+    val triangle_sets_per_perimeter =
+    for n <- (1 to max_perimeter)
+        yield
+        (n, triangle_number_counts.count(_ == n))
+    
+    println(triangle_sets_per_perimeter.maxBy(_(1)))
             
         
-
-
-def is_square(n : Int) : Boolean =
+def int_sqrt(n : Int) : List[Int] =
     val sqrt_n = math.sqrt(n)
     val lower = math.floor(sqrt_n).toInt
     val higher = math.ceil(sqrt_n).toInt
-    lower*lower == n || higher*higher == n
+
+    if lower*lower == n then
+        List(lower)
+    else if higher*higher == n then
+        List(higher)
+    else 
+        List()
