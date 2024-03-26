4d92b2b4ba5c22a86b9d8459bccf5e266e48f0c7
Steve Hussung <steven.hussung@gmail.com>
2024-03-25 23:46:46 -0400

    It runs, but not quickly enough to produce an answer in reasonable time.

--- src/main/scala/problem_41_pandigital_prime.scala
+++ src/main/scala/problem_41_pandigital_prime.scala
@@ -11,9 +11,19 @@
     println(greatest_pandigital_prime)
     */
    
+    //Attempt 2 may also be too slow somehow! Definitely thought this would work.
+    
+    //Attempt 3: Unified function to produce only prime pandigitals. How? Check as you go? Is that better?
+    //Attempt 3b: Tail recursion. Is it possible / desireable here?
+    
+   
     //Next step: make function to generate the pandigital numbers one by one in descending order. Then filter those through primes
     //(Step after that would be to skip the evens maybe? Unsure)
-    println(generate_pandigital("123"))
+    val digits = "123456789".sorted.reverse
+    val pandigital_numbers =  generate_pandigital(digits).map(_.toInt)
+    println(pandigital_numbers.filter(is_prime).head)
+    
+    
 
 //Later I may make this tail callable
 def generate_pandigital(s : String): List[String]= 
@@ -23,24 +33,18 @@ def generate_pandigital(s : String): List[String]=
         (
             for digit_char <- s
             digit_char_str = digit_char.toString
-            yield
             
-            //TO DO get this call to work. Need to make a map from the returned array to prepend the chosen digit. Will need to flatten as wel.
-            generate_pandigital(s.replace(digit_char_str, "")).map(
+            //TO DO get this call to work. Need to make a map from the returned array to prepend the chosen digit. Will need to flatten as well.
+            tail_string_list = generate_pandigital(s.replace(digit_char_str, ""))
+            yield
+                tail_string_list.map(
                 (partial_string) =>
                     digit_char_str + (partial_string)
-            ).toList()
-        )
+                )
+        ).flatten.toList
         
 
 
 def is_prime(n : Int) : Boolean =
     n > 1 &&
     (2 to n - 1).filter(n%_ == 0).isEmpty
-    
-/* //already defined!
-def is_pandigital(a : Int) : Boolean =
-    var digit_set = a.toString.toSet
-    digit_set.size == 9
-    &&  !digit_set.contains('0')
-*/
