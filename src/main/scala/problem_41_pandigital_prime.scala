79b8bb31cf15c600e2e76160c0abbe70ef687d9d
Steve Hussung <steven.hussung@gmail.com>
2024-03-25 23:47:36 -0400

    Figured it out! Made some adjustments to increase the speed--but in the end the problem was an innaccurate reading of the problem and a lack of mathematical insight. Turns out there are no 9-pandigital or 8-pandigital primes, and for an obvious reason.

--- src/main/scala/problem_41_pandigital_prime.scala
+++ src/main/scala/problem_41_pandigital_prime.scala
@@ -1,3 +1,6 @@
+
+val non_prime_flag = "-"
+
 @main def problem_41 : Unit = 
     println("guitar!")
     
@@ -13,22 +16,37 @@
    
     //Attempt 2 may also be too slow somehow! Definitely thought this would work.
     
-    //Attempt 3: Unified function to produce only prime pandigitals. How? Check as you go? Is that better?
-    //Attempt 3b: Tail recursion. Is it possible / desireable here?
+    //Attempt 2 is actually ok! The thing is that there are NO 9-digit pandigital primes! Why not? Gotta be something.
+    //Lmao. The triangular number for 9 is 9*10/2 = 45, which is divisible by 3. So they're always divisible by 3.
+    // Leaving this here, in memoriam
+    // val digits = "123456789".sorted.reverse
+       
+    // How about 8? Nope! Digit sum is just 9 less than 45, which is still divisible by 3.
+    // val digits = "12345678".sorted.reverse
     
    
     //Next step: make function to generate the pandigital numbers one by one in descending order. Then filter those through primes
     //(Step after that would be to skip the evens maybe? Unsure)
-    val digits = "123456789".sorted.reverse
-    val pandigital_numbers =  generate_pandigital(digits).map(_.toInt)
-    println(pandigital_numbers.filter(is_prime).head)
+    val digits = "1234567".sorted.reverse
+    val pandigital_numbers =  generate_pandigital(digits)
+        .filter(!_.contains("-"))
+        .map(_.toInt)
+    
+    println("Numbers generated! Now need the prime")
+    println(pandigital_numbers.dropWhile(!is_prime(_)).head)
     
     
 
 //Later I may make this tail callable
 def generate_pandigital(s : String): List[String]= 
+    /*
     if s.length == 0 then
         List("")
+        */
+    if s.length == 1 then
+        s match
+            case "2" | "4" | "5" | "6" | "8" => List(non_prime_flag)
+            case _ => List(s)
     else
         (
             for digit_char <- s
@@ -46,5 +64,7 @@ def generate_pandigital(s : String): List[String]=
 
 
 def is_prime(n : Int) : Boolean =
+    val result = 
     n > 1 &&
-    (2 to n - 1).filter(n%_ == 0).isEmpty
+    (2 to Math.sqrt(n).toInt + 1).filter(n%_ == 0).isEmpty
+    result
