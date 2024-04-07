b81f41a5bc40351e40f2fe51ba283f31093f2d9e
Steve Hussung <shussung@indot.in.gov>
2024-04-07 00:40:52 -0400

    Runs, possibly even correctly, but isn't finding the first instance of the solution quickly enough. I think the is_pentagonal method is the culprit.

--- src/main/scala/problem_44_pentagon.scala
+++ src/main/scala/problem_44_pentagon.scala
@@ -12,13 +12,21 @@
     
     var sum_of_pair : Int = 0
     var difference_of_pair_index = 1
+    var difference_of_pair = nth_pentagonal(difference_of_pair_index)
     
     while
+        println(s"Testing n = ${difference_of_pair_index}, D = ${difference_of_pair}")
         difference_of_pair = nth_pentagonal(difference_of_pair_index)
         !pentagonal_pair_exists(difference_of_pair)
     do
         difference_of_pair_index += 1
 
+    println(s"index of the difference: ${difference_of_pair_index}")
+    println(s"D = ${difference_of_pair}")
+    
+    var solution_pair = find_pentagonal_pairs(difference_of_pair)
+    println(s"Solution pair: ${solution_pair}")
+    // println(s"Sum of solution pair: ${solution_pair.toList.sum}")
 
 def nth_pentagonal(n : Int) : Int =
     n*(3*n-1)/2
@@ -29,7 +37,7 @@ def is_pentagonal(n : Int) : Boolean =
         seeker = seeker + 1
     nth_pentagonal(seeker) == n
 
-/* Commenting out for now. This is log(n), but sqrt(n) is still pretty good */
+/* If we bisect, we would achieve log(n), but sqrt(n) is still pretty good */
 
 def pentagonal_pair_exists(d : Int) : Boolean = 
     !find_pentagonal_pairs(d).isEmpty
@@ -47,7 +55,9 @@ def find_pentagonal_pairs(d : Int) =
     After this n, all pentagonal numbers are too far apart for this pentagonal difference.
     */
     for a <- (1 to d/3)
-        b <- (1 to d/3 + 1)
-        pentagonal_sum = nth_pentagonal(a) + nth_pentagonal(b)
-        if is_pentagonal(pentagonal_sum)
-        yield (nth_pentagonal(a), nth_pentagonal(b))
+        b <- (a to d/3 + 1)
+        p_a = nth_pentagonal(a)
+        p_b = nth_pentagonal(b)
+        pentagonal_sum = p_a + p_b
+        if p_b - p_a == d && is_pentagonal(pentagonal_sum)
+        yield (p_a, p_b)
