2635ea5dfcda9c1fb91136220a0b0d7dbc6cdba2
Steve Hussung <steven.hussung@gmail.com>
2025-11-07 17:01:40 -0500

    Refactoring to compute subsequence sums more quickly.

--- python_problems/problem_050.py
+++ python_problems/problem_050.py
@@ -19,27 +19,40 @@ def primes_under(n):
     primes = map(lambda x : x[0], filter(lambda x : x[1], list(enumerate(is_prime))))
     return list(primes)[2:]
 
+if primes_under(100) != [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
+    print("Error in primes_under")
+
 
 def longest_prime_sum_from_start(primes):
 
+    largest_prime = primes[-1]
     subsequences = [primes[0:i] for i in range(1, len(primes))]
-    sums = [sum(s) for s in subsequences]
+
+    #sums = [sum(s) for s in subsequences], but with early exit
+    sums = []
+    for s in subsequences:
+        temp_sum = sum(s)
+        if temp_sum > largest_prime:
+            break
+        else:
+            sums.append(temp_sum)
+
 
     prime_sum_subsequences = list(filter(lambda x : sum(x) in primes, subsequences))
 
     if len(prime_sum_subsequences) > 0:
         longest_length = max([len(s) for s in prime_sum_subsequences])
         longest_prime_sum = list(filter(lambda s : len(s) == longest_length, prime_sum_subsequences))
-
-        
         return longest_prime_sum[0]
-
     else:
         return []
 
+if longest_prime_sum_from_start([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]) != [2, 3, 5, 7, 11, 13]:
+    print("Error in longest_prime_sum_from_start")
+    print("Longest_prime_sum_from_start(primes(100)):", longest_prime_sum_from_start(primes_under(100)))
+
 
 def longest_prime_sum(primes):
-
     max_len = 0
     seq_with_max_len = []
     for i in range(len(primes)):
@@ -52,9 +65,7 @@ def longest_prime_sum(primes):
 
 
 n = 10000
-
 primes = primes_under(n)
-
 s = longest_prime_sum(primes)
 print(s)
 
